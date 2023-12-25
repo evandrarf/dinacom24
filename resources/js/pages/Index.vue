0de4f6fdf495444b1bdd94e22f26b5fd99ca11c7
Evandra Raditya <evandraraditya049@gmail.com>
2023-12-25 18:05:39 +0700

    feat: setup initial state

---
+++ resources/js/pages/Index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1 class="text-5xl text-red-600">Hello World!</h1>
+    </div>
+</template>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title inertia>{{ config('app.name', '') }}</title>
-    <link rel="shortcut icon" href="/assets/logo.png" type="image/png">
+    {{--
+    <link rel="shortcut icon" href="/assets/logo.png" type="image/png"> --}}
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
