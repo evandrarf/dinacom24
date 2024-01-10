2c6673c344f26b8ce7a3753d110ec484ffbd89d6
Evandra Raditya <evandraraditya049@gmail.com>
2024-01-10 21:38:58 +0700

    feat: ticket seeder

--- database/seeders/SocialAssistanceRecipientSeeder.php
+++ database/seeders/SocialAssistanceRecipientSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Actions\Utility\GenerateQrCode;
 use App\Models\Resident;
 use App\Models\SocialAssistance;
 use App\Models\SocialAssistanceRecipient;
+use App\Models\Ticket;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -23,12 +25,23 @@ class SocialAssistanceRecipientSeeder extends Seeder
             return $result === 'eligible';
         });
 
+        $generateQrCode = new GenerateQrCode();
+
         foreach ($social_assistances as $social_assistance) {
             $data = $filteredRecords->shuffle()->take(100)->toArray();
             foreach ($data as $recipient) {
-                SocialAssistanceRecipient::create([
+                $ticketNumber = time() . rand(1000000, 9999999);
+                $social_assistance_recipient = SocialAssistanceRecipient::create([
                     'social_assistance_id' => $social_assistance->id,
                     'resident_id' => $recipient['id'],
+                ]);
+                $qrCode = $generateQrCode->handle($ticketNumber);
+                Ticket::create([
+                    'social_assistance_id' => $social_assistance->id,
+                    'resident_id' => $recipient['id'],
+                    'ticket_number' => $ticketNumber,
+                    'qr_code_file_id' => $qrCode->id,
+                    'social_assistance_recipient_id' => $social_assistance_recipient->id,
                 ]);
             }
         }
