8ca9d7b8180f8903f9203a24471f3ad789741111
Evandra Raditya <evandraraditya049@gmail.com>
2024-01-14 14:25:09 +0700

    refactor: change json response social assistance

--- app/Http/Resources/Api/SocialAssistance/SocialAssistanceListResource.php
+++ app/Http/Resources/Api/SocialAssistance/SocialAssistanceListResource.php
@@ -2,33 +2,36 @@
 
 namespace App\Http\Resources\Api\SocialAssistance;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class SocialAssistanceListResource extends JsonResource
+class SocialAssistanceListResource extends ResourceCollection
 {
-    private $message;
-
-    public function __construct($resource, $message)
-    {
-        parent::__construct($resource);
-        $this->message = $message;
-        $this->resource = $resource;
-    }
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
-     */
     public function toArray($request)
     {
         return [
-            'data' => $this->resource,
+            'data' => $this->transformCollection($this->collection),
             'meta' => [
-                'success' => true,
-                'message' => $this->message,
-            ],
+                "success" => true,
+                "message" => "Sukses mendapatkan data bantuan sosial",
+            ]
         ];
+    }
+
+    private function transformData($data, $key)
+    {
+        Carbon::setLocale('id');
+        return [
+            'month' => Carbon::parse($key)->isoFormat('MMMM Y'),
+            'social_assistance' => $data
+        ];
+    }
+
+    private function transformCollection($collection)
+    {
+        return $collection->transform(function ($data, $key) {
+            return $this->transformData($data, $key);
+        })->values();
     }
 }
